e2f5cb3cd26903712a759b2cda71249b94ce87d3
victor <vjbsh@hotmail.com>
2025-02-18 22:50:34 -0600

    oh

--- main.py
+++ main.py
@@ -8,11 +8,20 @@ def transcribe_audio():
     This function opens a file dialog to select an audio file, loads a pre-trained model, and calls the transcribe_audio_helper
     function to transcribe the selected audio file. It also displays a charging icon while the transcription is being processed.
     """
-    filepath = filedialog.askopenfilename(title="Select audio file", filetypes=[("WAV files", "*.wav")])
+    filepath = filedialog.askopenfilename(
+        title="Select audio file",
+        filetypes=[
+            ("Audio files", "*.wav *.mp3 .m4a"),
+            ("M4A files", "*.m4a"),
+            ("WAV files", "*.wav"),
+            ("MP3 files", "*.mp3"),
+        ],
+    )
     if filepath:
         model = whisper.load_model(name="base", download_root="models")
         progress_bar.pack(fill=tk.X, padx=10)
         root.after(1000, transcribe_audio_helper, model, filepath)
+
 
 def transcribe_audio_helper(model, filepath):
     """
@@ -21,7 +30,7 @@ def transcribe_audio_helper(model, filepath):
     message is displayed in a messagebox. Finally, the charging icon is hidden.
     """
     try:
-        result = model.transcribe(filepath)
+        result = model.transcribe(filepath, fp16=False)
         text_output.config(state=tk.NORMAL)
         text_output.delete("1.0", tk.END)
         text_output.insert(tk.END, "".join(result["text"]))
@@ -34,6 +43,7 @@ def transcribe_audio_helper(model, filepath):
     finally:
         progress_bar.pack_forget()
 
+
 root = tk.Tk()
 root.title("Speech to Text")
 
@@ -43,7 +53,9 @@ button_select.pack(pady=10)
 text_output = tk.Text(root, height=10, state=tk.DISABLED)
 text_output.pack(padx=10, pady=10)
 
-progress_bar = ttk.Progressbar(root, orient=tk.HORIZONTAL, length=200, mode='determinate')
+progress_bar = ttk.Progressbar(
+    root, orient=tk.HORIZONTAL, length=200, mode="determinate"
+)
 progress_bar.pack_forget()
 
 root.mainloop()
